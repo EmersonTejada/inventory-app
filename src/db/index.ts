335d80b850b520e26cf0eb7ff4e8fc8190059e5b
EmersonTejada <emerson.8965@gmail.com>
2025-10-24 23:02:09 -0400

    refactor: Update database connection configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,5 @@
 import { Pool } from "pg"
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT)
-})
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false }, 
+});
